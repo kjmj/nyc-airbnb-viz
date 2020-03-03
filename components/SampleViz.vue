687d1fedb9926c5b52ec0de6bae0781ee551de73
Ken Morton Jr <kmorton15@gmail.com>
2020-03-03 16:21:38 -0500

    created quick colored svg

--- components/SampleViz.vue
+++ components/SampleViz.vue
@@ -2,10 +2,13 @@
   <div>
     <h2 class="title is-2">Sample Viz</h2>
     <h3 class="subtitle">Sample Viz will go here</h3>
+    <div id="sampleViz"></div>
   </div>
 </template>
 
 <script>
+  import * as d3 from 'd3'
+
   export default {
     name: "SampleViz",
     props: {
@@ -14,10 +17,26 @@
     mounted() {
       let vm = this
 
+      // load data, then draw graph
       vm.datasetPromise.then(function(data) {
+        // data available here
         console.log(data)
-      })
 
+        var width = 940
+        var height = 600
+
+        // draw a quick svg for show
+        var svg = d3
+          .select('#sampleViz')
+          .append('svg')
+          .attr('width', width)
+          .attr('height', height)
+
+        svg.append("rect")
+          .attr("width", "100%")
+          .attr("height", "100%")
+          .attr("fill", "#4099FF");
+      })
     }
   }
 </script>
