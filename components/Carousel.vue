1bfc0c1be252b4ecb7e473229ada79dbca21bd33
Ken Morton Jr <kmorton15@gmail.com>
2020-03-05 01:02:00 -0500

    changed carousel items

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -38,33 +38,33 @@
         indicatorStyle: 'is-dots',
         carousels: [
           {
-            title: 'Luxury 1 bedroom apt. -stunning Manhattan views',
-            link: 'https://www.airbnb.com/rooms/13894339?source_impression_id=p3_1583297353_H%2F4qsqKVYqY2qB2k',
-            image: 'https://a0.muscache.com/im/pictures/4938ff43-2416-48bd-b4c6-3c38f4115fb4.jpg?aki_policy=xx_large',
-            borough: 'Brooklyn',
-            neighborhood: 'Greenpoint',
-            price: '$10000 / night'
+            title: 'Greenwich Village Townhome with Private Garden!',
+            link: 'https://www.airbnb.com/rooms/20692563?source_impression_id=p3_1583387651_iq%2FaHQq8pMMRjMyc',
+            image: 'https://a0.muscache.com/im/pictures/5d3b16c4-5de6-439c-bceb-a0fafe3411cf.jpg?aki_policy=x_large',
+            borough: 'Manhattan',
+            neighborhood: 'Greenwich Village',
+            price: '$1000 / night'
           },
           {
-            title: '1-BR Lincoln Center',
-            link: 'https://www.airbnb.com/rooms/22436899?source_impression_id=p3_1583297398_lBhjvGDuV5nkwy0v',
-            image: 'https://a0.muscache.com/im/pictures/e3571de1-a97b-447e-9ada-6b83eddb8700.jpg?aki_policy=xx_large',
+            title: 'West Chelsea Studios - Penthouse 12',
+            link: 'https://www.airbnb.com/rooms/18071399?source_impression_id=p3_1583387936_WA4pxUakiyBP9OH4',
+            image: 'https://a0.muscache.com/im/pictures/dfa06cca-d8b1-4dd0-9816-e4dd129ae3ef.jpg?aki_policy=xx_large',
             borough: 'Manhattan',
-            neighborhood: 'Upper West Side',
-            price: '$10000 / night'
+            neighborhood: 'Chelsea',
+            price: '$2800 / night'
+          },
+          {
+            title: '70 Luxury MotorYacht on the Hudson',
+            link: 'https://www.airbnb.com/rooms/33007610?source_impression_id=p3_1583387311_GSQaQzx47UFNzQAm',
+            image: 'https://a0.muscache.com/im/pictures/84830680/93103707_original.jpg?aki_policy=xx_large',
+            borough: 'Manhattan',
+            neighborhood: 'Battery Park City',
+            price: '$6500 / night'
           },
           {
             title: 'Quiet, Clean, Lit @ LES & Chinatown',
             link: 'https://www.airbnb.com/rooms/9528920?source_impression_id=p3_1583297472_bCFqu%2BAlJNg9Wjuo',
             image: 'https://a0.muscache.com/im/pictures/fb030ec8-4e8f-4243-8cbe-99a89b226878.jpg?aki_policy=xx_large',
-            borough: 'Manhattan',
-            neighborhood: 'Lower East Side',
-            price: '$9999 / night'
-          },
-          {
-            title: '2br - The Heart of NYC: Manhattans Lower East Side',
-            link: 'https://www.airbnb.com/rooms/31340283?source_impression_id=p3_1583297490_HBgNLABgp%2B3I6fI%2B',
-            image: 'https://a0.muscache.com/im/pictures/643f3d26-61a6-4d7a-9c08-4647c9674a09.jpg?aki_policy=xx_large',
             borough: 'Manhattan',
             neighborhood: 'Lower East Side',
             price: '$9999 / night'
